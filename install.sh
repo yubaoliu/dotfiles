e738e8243c004316575f3c741a701d250acfa711
Yubao <yubaoliu89@gmail.com>
2021-04-12 21:11:26 +0900

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 sudo apt update
 
+echo "Install ohmyzsh"
+echo "https://ohmyz.sh/#install"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 echo "Install dependencies"
 sudo apt install -y trash-cli \
 libevent-dev \
@@ -14,11 +18,20 @@ vim-gtk  #vim clipboard
 
 echo "Install ruby"
 sudo apt-add-repository ppa:brightbox/ruby-ng && sudo apt-get -y update
-sudo apt install -y ruby2.6 ruby2.6-dev
+sudo apt install -y ruby ruby-dev
 
 # VIm plugin
 sudo apt-get install -y astyle clang-format python-autopep8  remark 
 sudo apt install -y npm && npm install -g remark-cli
+
+sudo apt install python3-pip
 pip install autopep8
 pip install yapf 
 
+
+#  Un-share the history among terminals
+unsetopt share_history
+setopt no_share_history
+
+# mkdir
+echo -e "function mkcd(){mkdir -p \$1 && cd \$1}" >> ~/.zshrc && exec $SHELL
